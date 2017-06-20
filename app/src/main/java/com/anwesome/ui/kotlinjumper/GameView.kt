30c32f9e8e3b05d29636bd334616245dc7d99096
Anwesh43 <anweshthecool0@gmail.com>
2017-06-21 00:54:47 +0530

    Adding AnimationHandler

--- app/src/main/java/com/anwesome/ui/kotlinjumper/GameView.kt
+++ app/src/main/java/com/anwesome/ui/kotlinjumper/GameView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.RectF
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -16,15 +17,26 @@ class GameView(context:Context):View(context) {
     private var w:Int = 0
     private var h:Int = 0
     private var ball:Ball = Ball(0.0f)
+    private var animationHandler:AnimationHandler = AnimationHandler(this,ball)
     override fun onDraw(canvas:Canvas) {
         if(time == 0) {
             w = canvas.width
             h = canvas.height
             ball = Ball((2*h/3)*1.0f)
+            animationHandler = AnimationHandler(this,ball)
         }
         canvas.drawColor(Color.BLACK)
         ball.draw(canvas,paint,w*1.0f)
         time++
+        animationHandler.animate()
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                animationHandler.startAnimating()
+            }
+        }
+        return true
     }
     data class Ball(var y:Float,var rot:Float=0.0f,var dir:Float=0.0f) {
         fun draw(canvas:Canvas,paint:Paint,w:Float) {
@@ -38,13 +50,12 @@ class GameView(context:Context):View(context) {
             canvas.restore()
         }
         fun jump(){
-            y+=dir*10
-            rot+=dir*10;
-            if(rot == -180.0f) {
-                dir *= -1
-            }
-            if(rot == 0.0f) {
-                dir = 0.0f
+            y+=dir*20
+            rot+=dir*20;
+            dir = when(rot) {
+                -180.0f -> 1.0f
+                0.0f -> 0.0f
+                else -> dir
             }
         }
         fun startJumping() {
@@ -54,4 +65,34 @@ class GameView(context:Context):View(context) {
         }
     }
 }
+class AnimationHandler(var view:GameView,var ball:GameView.Ball) {
+    private var isAnimated:Boolean = false
+    fun animate() {
+        when(isAnimated) {
+             true -> {
+                    ball.jump()
+                    when(ball.dir) {
+                        0.0f->{
+                            isAnimated = false
+                        }
+                    }
+                    try {
+                        Thread.sleep(50);
+                        view.invalidate()
+                    }
+                    catch (ex:Exception) {
 
+                    }
+             }
+        }
+    }
+    fun startAnimating() {
+        when(isAnimated) {
+            false -> {
+                isAnimated = true
+                ball.startJumping()
+                view.postInvalidate()
+            }
+        }
+    }
+}
